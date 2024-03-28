d19be03ba732cc504975130e83dc583c63007353
PREM KUMAR PATHAK <wdesign332@gmail.com>
2024-03-28 18:16:41 +0530

    day 02 task

--- script.js
+++ script.js
@@ -1,66 +1,109 @@
-function lable_create(tagname, attr1, attrval, content) {
-  var ele = document.createElement(tagname);
-  ele.setAttribute(attr1, attrval);
+function lable_create(tagname, attr, attrval, content) {
+  let ele = document.createElement(tagname);
+  ele.setAttribute(attr, attrval);
   ele.innerHTML = content;
   return ele;
 }
 
 function break_create() {
-  var break_ele = document.createElement("br");
+  let break_ele = document.createElement("br");
   return break_ele;
 }
 
-function input_create(tagname, attr1, attrval, idname) {
-  var input_ele = document.createElement(tagname);
-  input_ele.setAttribute(attr1, attrval);
+function input_create(tagname, attr1, attrval1, idname) {
+  let input_ele = document.createElement(tagname);
+  input_ele.setAttribute(attr1, attrval1);
   input_ele.id = idname;
   return input_ele;
 }
 
-var first_name = lable_create("lable", "for", "fname", "First Name");
+function submit_button(
+  tagname,
+  attr,
+  attrval,
+  attr1,
+  attrval1,
+  attrfn,
+  attrfnval
+) {
+  let sub_ele = document.createElement(tagname);
+  sub_ele.setAttribute(attr, attrval);
+  sub_ele.setAttribute(attr1, attrval1);
+  sub_ele.setAttribute(attrfn, attrfnval);
+  return sub_ele;
+}
+
+let first_name_ele = lable_create("lable", "for", "first_name", "First Name");
+let first_name_break = break_create();
+let input_first_name = input_create(
+  "input",
+  "type",
+  "first_name",
+  "first_name"
+);
+let input_first_name_break = document.createElement("br");
+
+let last_name_ele = lable_create("lable", "for", "last_name", "Last Name");
+let last_name_break = break_create();
+let input_last_name = input_create("input", "type", "last_name", "last_name");
+let input_last_name_break = document.createElement("br");
+
+let email_ele = lable_create("lable", "for", "email", "Email");
 let lable_break = break_create();
-let input_fname = input_create("input", "type", "first_name", "fname");
-let input_break = break_create();
-
-var last_name = lable_create("lable", "for", "fname", "Last Name");
-let lname_break = break_create();
-let input_lname = input_create("input", "type", "first_name", "fname");
-let lname_break1 = break_create();
-
-var email = lable_create("lable", "for", "email", "Email");
-let email_break = break_create();
 let input_email = input_create("input", "type", "email", "email");
-let email_break1 = break_create();
+let input_break = document.createElement("br");
 
-var pass = lable_create("lable", "for", "pass", "Password");
-let pass_break = break_create();
+let pass_ele = lable_create("lable", "for", "pass", "Password");
+let pass_lable_break = break_create();
 let input_pass = input_create("input", "type", "pass", "pass");
-let pass_break1 = break_create();
+let pass_input_break = document.createElement("br");
 
-var add = lable_create("lable", "for", "add", "Address");
-let add_break = break_create();
-let input_add = input_create("input", "type", "add", "add");
-let add_break1 = break_create();
+let add_element = lable_create("lable", "for", "address", "Address");
+let address_lable_break = break_create();
+let input_address = input_create("input", "type", "address", "address");
+let address_input_break = break_create();
+
+let submit_input = submit_button(
+  "input",
+  "type",
+  "submit",
+  "value",
+  "submit",
+  "onclick",
+  "submitfun()"
+);
+
+function submitfun() {
+  let fname = document.getElementById("first_name").value;
+  console.log(fname);
+  let lname = document.getElementById("last_name").value;
+  console.log(lname);
+  let email_t = document.getElementById("email").value;
+  console.log(email_t);
+  let add_t = document.getElementById("address").value;
+  console.log(add_t);
+}
 
 document.body.append(
-  first_name,
+  first_name_ele,
+  first_name_break,
+  input_first_name,
+  input_first_name_break,
+  last_name_ele,
+  last_name_break,
+  input_last_name,
+  input_last_name_break,
+  email_ele,
   lable_break,
-  input_fname,
-  input_break,
-  last_name,
-  lname_break,
-  input_lname,
-  lname_break1,
-  email,
-  email_break,
   input_email,
-  email_break1,
-  pass,
-  pass_break,
+  input_break,
+  pass_ele,
+  pass_lable_break,
   input_pass,
-  pass_break1,
-  add,
-  add_break,
-  input_add,
-  add_break1
+  pass_input_break,
+  add_element,
+  address_lable_break,
+  input_address,
+  address_input_break,
+  submit_input
 );
